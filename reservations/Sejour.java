ec5b6439fd6f0b417b0f00a57f9bb0eca9538bf8
aubertMal <malek.aubert@gmail.com>
2021-01-07 10:57:06 +0100

    implémentation d'une interface sejour

--- reservations/Sejour.java
+++ reservations/Sejour.java
@@ -3,7 +3,9 @@ package aubert.airbnb.reservations;
 import aubert.airbnb.logements.Logement;
 import aubert.airbnb.outils.MaDate;
 
-public class Sejour {
+import java.util.Date;
+
+public class Sejour implements SejourInterface{
     private MaDate dateArrivee;
     private int nbNuits;
     private Logement logement;
@@ -16,9 +18,32 @@ public class Sejour {
         nbVoyageurs = nombreVoyageurs;
     }
 
+
+    /**
+     * Cette méthode vérifie si le nombre de voyageurs ne dépaasent pas la capacité d'accueil
+     * @return true or false
+     */
+
+    public boolean verificationNombreDeVoyageurs(){
+        return nbVoyageurs <= logement.getNbVoyageursMax() && nbVoyageurs > 0;
+    }
+
+    public boolean verificationNombreDeNuits(){
+        return (nbNuits >= 1 && nbNuits <= 31);
+    }
+
+    /**
+     * Cette méthode prend en paramètres une date de séjour et vérifie si c'est après la date actuelle
+     * @return true or false
+     */
+    public boolean verificationDateArrivee(){
+        return dateArrivee.after(new Date());
+    }
+
     public void afficher(){
         logement.afficher();
         System.out.println("La date d'arrivée est le " + dateArrivee.toString(dateArrivee));
         System.out.println("Le prix de ce séjour est de " + logement.getTarifParNuit() + '€');
     }
 }
+
---
+++ reservations/SejourInterface.java
@@ -0,0 +1,30 @@
+package aubert.airbnb.reservations;
+
+import aubert.airbnb.outils.MaDate;
+
+import java.util.Date;
+
+public interface SejourInterface {
+
+    /**
+     * Cette méthode prend en paramètres une date de séjour et vérifie si c'est après la date actuelle
+     * @return true or false
+     */
+
+    static boolean verificationDateArrivee(){
+        return true;
+    }
+
+    static boolean verificationNombreDeNuits(){
+        return true;
+    }
+
+    static boolean verificationNombreDeVoyageurs(){
+        return true;
+    };
+
+    static void afficher(){
+
+    }
+
+}
