798a5426687abd9127c057bee6d8e3e1a0d55ee8
aubertMal <malek.aubert@gmail.com>
2021-02-01 13:38:55 +0100

    feat: parser XML pour enregistrer Hotes et Apparts

--- menu/Menu.java
+++ menu/Menu.java
@@ -1,12 +1,31 @@
 package aubert.airbnb.menu;
+import aubert.airbnb.logements.Appartement;
+import aubert.airbnb.logements.Logement;
+import aubert.airbnb.logements.Maison;
+import aubert.airbnb.reservations.Reservation;
 import aubert.airbnb.utilisateurs.Hote;
+import aubert.airbnb.utilisateurs.Voyageur;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Menu {
 
     static Scanner scanner;
+    static File xmlFile = new File("C:/Workspace/logements.xml");
+
+    static ArrayList<Hote> listHotes;
+    static ArrayList<Logement> listLogements;
+    static ArrayList<Voyageur> listVoyageurs;
+    static ArrayList<Reservation> listReservations;
+
 
 
     public static void main(String[] args) {
@@ -14,14 +33,118 @@ public class Menu {
 
         scanner = new Scanner (System.in);
 
-        listerMenu();
-        chargerFichier();
+        System.out.println("Voulez vous charger des listes d'un fichier?");
+        System.out.println(" 1 : oui");
+        System.out.println(" 2 : non");
+        System.out.println(" 3: Annuler");
+        int fromXml = scanner.nextInt();
+        switch (fromXml){
+            case 1:
+                initFromXml(xmlFile);
+                break;
+            case 2:
+                init();
+                break;
+            case 3:
+                break;
+        }
+
+        //listerMenu();
 
         scanner.close();
     }
 
-    private static void chargerFichier() {
-        
+    private static void init() {
+        listHotes = new ArrayList<>();
+        listLogements = new ArrayList<>();
+        listVoyageurs = new ArrayList<>();
+        listReservations = new ArrayList<>();
+
+        // Création des Hotes
+        Hote hote1 = new Hote("Peter", "Bardu", 28, 12);
+        Hote hote2 = new Hote("Patrick", "Martin", 32, 12);
+        Hote hote3 = new Hote("Jeanne", "Voisin", 26, 24);
+        Hote hote4 = new Hote("Maurice", "Meunier", 46, 12);
+
+        listHotes.add(hote1);
+        listHotes.add(hote2);
+        listHotes.add(hote3);
+        listHotes.add(hote4);
+
+        // Création de Logement
+        Maison maison1 = new Maison(hote1, 40, "18 Bis rue Romain Rolland, 37230 Fondettes", 140, 2, 500, true);
+        Maison maison2 = new Maison(hote1, 35, "146 Rue George Sand, 59553 Cuincy", 120, 2, 200, false);
+        Maison maison3 = new Maison(hote1, 60, "13 Rue de la Liberté, 62800 Liévin", 90, 4, 2000, true);
+        Appartement appartement1 = new Appartement(hote1, 35, "46 Rue des Canonniers, 59800 Lille", 72, 2, 3, 20);
+        Appartement appartement2 = new Appartement(hote1, 35, "111 Rue Colbert, 37000 Tours", 42, 2, 2, 0);
+
+        listLogements.add(maison1);
+        listLogements.add(maison2);
+        listLogements.add(maison3);
+        listLogements.add(appartement1);
+        listLogements.add(appartement2);
+
+        // Création de voyageurs
+        Voyageur voyageur1 = new Voyageur("Alain", "Favre", 54);
+        Voyageur voyageur2 = new Voyageur("Maxime", "Albert", 29);
+
+        listVoyageurs.add(voyageur1);
+        listVoyageurs.add(voyageur2);
+    }
+
+    private static void initFromXml(File xmlFile) {
+        listHotes = new ArrayList<>();
+        listLogements = new ArrayList<>();
+
+        try {
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(xmlFile);
+
+            doc.getDocumentElement().normalize();
+            String root = doc.getDocumentElement().getNodeName();
+
+            NodeList nListAppartements = doc.getElementsByTagName("Appartement");
+            NodeList nListMaisons = doc.getElementsByTagName("Maison");
+
+            //Enregistrer les Apparts
+            for (int idxAppart = 0; idxAppart< nListAppartements.getLength(); idxAppart++){
+                Node nNode = nListAppartements.item(idxAppart);
+
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+                    //enregistrer le Hote
+                    Node nodeHote = nNode.getFirstChild();
+                    Element hoteElement = (Element) nodeHote;
+
+                    Hote nouveauHote = new Hote(hoteElement.getAttribute("prenom"),
+                            hoteElement.getAttribute("nom"),
+                            Integer.parseInt(hoteElement.getAttribute("age")),
+                            Integer.parseInt(hoteElement.getAttribute("delaiResponse")));
+                    nouveauHote.afficher();
+
+                    Menu.listHotes.add(nouveauHote);
+
+                    //enregistrer l'appartement
+                    Element eElement = (Element) nNode;
+
+                    //(Hote proprietaire, int tarifNuit, String adresseLogement, int superficieLogement, int nombreVoyageursMax, int numEtage,int surfaceBalcon)
+                    Appartement nouvelAppart = new Appartement(nouveauHote,
+                                                    Integer.parseInt(eElement.getAttribute("tarifParNuit")),
+                                                    eElement.getAttribute("adresse"),
+                                                    Integer.parseInt(eElement.getAttribute("superficie")),
+                                                    Integer.parseInt(eElement.getAttribute("nbVoyageyrsMax")),
+                                                    Integer.parseInt(eElement.getAttribute("numeroEtage")),
+                                                    Integer.parseInt(eElement.getAttribute("superificieBalcon")));
+                    Menu.listLogements.add(nouvelAppart);
+                    nouvelAppart.afficher();
+                }
+
+            }
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     static void listerMenu(){
@@ -44,6 +167,17 @@ public class Menu {
             case 1:
                 GestionHotes.listerHotes();
                 break;
+            case 2:
+                GestionLogements.listerLogements();
+                break;
+            case 3:
+                GestionVoyageurs.listerVoyageurs();
+                break;
+            case 4:
+                GestionReservations.listerReservations();
+                break;
+            case 5:
+                System.out.println("A bientôt!");
             default:
                 break;
         }
